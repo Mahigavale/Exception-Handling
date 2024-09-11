9c9df38ec72b0c22ab448366b3f63564152aafb2
Mahigavale <Maheshgavale07@gmail.com>
2024-09-11 19:32:50 +0530

    the miscellenious

---
+++ CollectionFramerwork/src/collectionFramerwroks/ArranegZero2.java
@@ -0,0 +1,38 @@
+package collectionFramerwroks;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class ArranegZero2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
+
+
+@FunctionalInterface
+interface Demo{
+	
+	void demor();
+	
+	public default void show()
+	{
+		ArrayList<Integer> numbers=new ArrayList<Integer>();
+		numbers.add(1);
+		numbers.add(3);
+		numbers.add(4);
+		numbers.add(5);
+		
+
+
+	//numbers.forEach((n)->{System.out.println(n);});
+		 numbers.forEach( (n) -> { System.out.println(n); } );
+		 
+		 Consumer<Integer> method=(n)->{System.out.println(n);}
+	
+}
+}
