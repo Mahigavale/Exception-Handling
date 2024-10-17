12c546002bc890cd80faf08448afc44bd87a067e
Mahigavale <Maheshgavale07@gmail.com>
2024-10-17 09:08:13 +0530

    springboot

--- demo/demo/src/main/java/com/example/demo/Controller/Democontroller.java
+++ demo/demo/src/main/java/com/example/demo/Controller/Democontroller.java
@@ -1,6 +1,9 @@
 package com.example.demo.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,7 +48,44 @@ public class Democontroller {
 		repo.save(address);
 		
 		
+		
 		return "address saved sucessfully !";
+	}
+	
+	
+	// Use Case:=> find all addresses in db. FindAll()=>returns all the addresses.
+	
+	@GetMapping("/get-all")
+	public List<Address> findalladd()
+	{
+	
+		return repo.findAll();
+		
+
+	}
+	//Use case :=>I want a specific address.
+	
+	
+	@GetMapping("/get-by-id")
+	public Address getbyid(@RequestParam("id") int id)
+	{
+		return repo.findById(id).get();
+		//error
+	}
+	
+	
+	@DeleteMapping("/delete-by-id")
+	public String deletebyid(@RequestParam("id") int id)
+	{
+		repo.deleteById(id);
+		
+		return "Done ! deleted sucessfully !";
+	}
+	
+	@GetMapping("/get-by-pincode")
+	public Address getbypn(@RequestParam("pn") String pn)
+	{
+	return repo.getbypincode(pn);
 	}
 	
 	
--- demo/demo/src/main/java/com/example/demo/Repository/AddressRepo.java
+++ demo/demo/src/main/java/com/example/demo/Repository/AddressRepo.java
@@ -1,6 +1,8 @@
 package com.example.demo.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.Entity.Address;
@@ -8,4 +10,12 @@ import com.example.demo.Entity.Address;
 @Repository
 public interface AddressRepo  extends JpaRepository<Address, Integer> {
 
+	//HQL / SQL
+	
+	
+	@Query(value="select * from address where pincode= :pn",nativeQuery=true)
+	public Address getbypincode(@Param("pn") String pn);
+	
+	//extensibility
+	//reusability
 }
