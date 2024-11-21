8fa3a5e46b7badb32eb3646d694d5f2b6ad8e36a
Mahigavale <Maheshgavale07@gmail.com>
2024-11-21 14:49:45 +0530

    java

--- myanatomy/src/myanatomy/Democosnumer.java
+++ myanatomy/src/myanatomy/Democosnumer.java
@@ -1,5 +1,7 @@
 package myanatomy;
 
+import java.util.Scanner;
+
 public class Democosnumer {
 
 	public static void main(String[] args) {
@@ -10,7 +12,42 @@ public class Democosnumer {
 		System.out.println(cal.add(12, 12));
 		
 		
+		
+		Scanner scan=new Scanner(System.in);
+		
+//		
+//		System.out.println("enter the values :");
+//		int n=scan.nextInt();
+//		//here the newline character is remained in this line 
+//		//so with the help of the scan.nextline();
+//		//we consume that newline character here.
+//		scan.nextLine();
+//		
+//		System.out.println("enter the string:");
+//
+//		String s=scan.nextLine();
+//		
+//		
+//		System.out.println(n);
+//
+//	
+//		System.out.println(s);
+		
+		
+		System.out.println("please enter the string:");
+		
+		int i=scan.nextInt();
+		 scan.nextLine();
+		 //new line character is stayed in that specific line so
+		 //to overcome it we use the nextLine() to consume it.
+		String str=scan.nextLine();
+		
+		
+		
+		System.out.println(str+" "+i);
 	}
+	
+	
 
 }
 
