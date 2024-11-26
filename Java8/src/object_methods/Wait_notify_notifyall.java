89e68c541f57b78ffce0ad855ad4de38f769d317
Mahigavale <Maheshgavale07@gmail.com>
2024-11-26 16:47:45 +0530

    java

--- Java8/src/object_methods/Wait_notify_notifyall.java
+++ Java8/src/object_methods/Wait_notify_notifyall.java
@@ -2,9 +2,40 @@ package object_methods;
 
 public class Wait_notify_notifyall {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
 
+		
+	Test t=new Test();
+	
+	synchronized(t)
+	{
+		t.start();
+		
+		t.wait(1000);
+		
+		System.out.println("main started !");
 	}
-
+	
+	System.out.println("main printed something !");
+	}
+	
 }
+
+class Test extends Thread {
+
+	
+	public void run() {
+		
+		synchronized(this) {
+			
+			System.out.println("test Thread Starting !");
+		for (int i = 0; i < 10; i++) 
+		{
+			System.out.println("output is :" + i);
+		}
+		System.out.println("notifying remaining threads !");
+		notifyAll();
+	}
+	}
+}
+
